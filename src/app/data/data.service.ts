72663ba1820fd324aa763b21ff190716071e41df
revolt268 <richard-delong@charter.net>
2021-05-21 10:39:37 -0500

    Posting and Data Access

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { UserSettings } from './user-settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getSubscriptionTypes(): Observable<string[]> {
+   return of(['Monthly', 'Annual', 'Lifetime']);
+}
+
+  postUserSettingsForm(userSettings: UserSettings) : Observable<any> {
+   return this.http.post('https://putsreq.com/zmZ0OUlEhyXbkbyLTL9X', userSettings);   
+   //return of(userSettings);
+
+  }
+}
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { DataService } from '../data/data.service';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -18,13 +20,35 @@ export class UserSettingsFormComponent implements OnInit {
    };
 
    userSettings : UserSettings = {...this.originalUserSettings };
-  constructor() { }
+   postError = false;
+   postErrorMessage = '';
+   subscriptionTypes: Observable<string[]> | undefined;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+     this.subscriptionTypes = this.dataService.getSubscriptionTypes();
+  }
+
+  onHttpError(errorResponse: any){
+     console.log('error: ', errorResponse);
+     this.postError = true;
+     this.postErrorMessage = errorResponse.error.errorMessage;     
   }
 
   onSubmit(form: NgForm) {
      console.log('in onSubmit: ', form.valid);
+
+     if (form.valid){
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+         result => console.log('success: ', result),
+         error => this.onHttpError(error)
+      );
+     }
+     else {
+        this.postError = true;
+        this.postErrorMessage = "Please fix the above errors";
+     }    
   }
 
   onBlur(field: NgModel)  {
